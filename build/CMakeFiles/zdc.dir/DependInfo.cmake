7d86f1f5b98c654afb8cfe7a80ec71f63223cab8
mphipps2 <mwp897@gmail.com>
2018-07-31 18:16:01 -0500

    Created

---
+++ build/CMakeFiles/zdc.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mike/ZDC_Standalone_copy/src/ActionInitialization.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/ActionInitialization.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/CherenkovHit.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/CherenkovHit.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/CherenkovSD.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/CherenkovSD.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/DetectorConstruction.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/DetectorConstruction.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/EventAction.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/EventAction.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/ModType1.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/ModType1.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/ModType2.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/ModType2.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/ModType3.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/ModType3.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/ModTypeCustom.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/ModTypeCustom.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/MyMTRunManager.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/MyMTRunManager.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/MyRunManager.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/MyRunManager.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/OpticalPhysics.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/OpticalPhysics.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/PhysicsList.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/PhysicsList.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/PrimaryGeneratorAction.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/QuartzHit.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/QuartzHit.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/QuartzSD.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/QuartzSD.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/RunAction.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/RunAction.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/SharedData.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/SharedData.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/StepMax.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/StepMax.cc.o"
+  "/home/mike/ZDC_Standalone_copy/src/SteppingAction.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/src/SteppingAction.cc.o"
+  "/home/mike/ZDC_Standalone_copy/zdc.cc" "/home/mike/ZDC_Standalone_copy/build/CMakeFiles/zdc.dir/zdc.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ANALYSIS_HH_"
+  "G4INTY_USE_QT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/geant4.10.02-install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "../include"
+  "/usr/local/root-6.04.00/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/zdc.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zdc.dir/zdc.cc.o"
+  "CMakeFiles/zdc.dir/src/EventAction.cc.o"
+  "CMakeFiles/zdc.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/zdc.dir/src/MyRunManager.cc.o"
+  "CMakeFiles/zdc.dir/src/QuartzHit.cc.o"
+  "CMakeFiles/zdc.dir/src/ModType2.cc.o"
+  "CMakeFiles/zdc.dir/src/MyMTRunManager.cc.o"
+  "CMakeFiles/zdc.dir/src/CherenkovSD.cc.o"
+  "CMakeFiles/zdc.dir/src/CherenkovHit.cc.o"
+  "CMakeFiles/zdc.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/zdc.dir/src/StepMax.cc.o"
+  "CMakeFiles/zdc.dir/src/ModTypeCustom.cc.o"
+  "CMakeFiles/zdc.dir/src/ModType3.cc.o"
+  "CMakeFiles/zdc.dir/src/SharedData.cc.o"
+  "CMakeFiles/zdc.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/zdc.dir/src/ModType1.cc.o"
+  "CMakeFiles/zdc.dir/src/OpticalPhysics.cc.o"
+  "CMakeFiles/zdc.dir/src/RunAction.cc.o"
+  "CMakeFiles/zdc.dir/src/QuartzSD.cc.o"
+  "CMakeFiles/zdc.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/zdc.dir/src/PhysicsList.cc.o"
+  "zdc.pdb"
+  "zdc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zdc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
