d3fcefef6b6fb28d84431179bc9b5dc5419b0b4f
mphipps2 <mwp897@gmail.com>
2018-07-31 18:21:03 -0500

    created

--- build/CMakeFiles/DEPENDS.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/DEPENDS"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DEPENDS.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
